56dd47ba1355478da4abafc4c4276067ac6e6678
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-05-16 20:34:36 +0300

    Update: created: new firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAKpqXlx8PmNZUGKairxjNprsrHXHQ4Ylk",
+    authDomain: "onlinepayments-4a216.firebaseapp.com",
+    databaseURL: "https://onlinepayments-4a216.firebaseio.com",
+    projectId: "onlinepayments-4a216",
+    storageBucket: "onlinepayments-4a216.appspot.com",
+    messagingSenderId: "528507565107",
+    appId: "1:528507565107:web:27e0990e7de103f2ebdf46",
+    measurementId: "G-9KX7SMX99J"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAKpqXlx8PmNZUGKairxjNprsrHXHQ4Ylk",
+    authDomain: "onlinepayments-4a216.firebaseapp.com",
+    databaseURL: "https://onlinepayments-4a216.firebaseio.com",
+    projectId: "onlinepayments-4a216",
+    storageBucket: "onlinepayments-4a216.appspot.com",
+    messagingSenderId: "528507565107",
+    appId: "1:528507565107:web:27e0990e7de103f2ebdf46",
+    measurementId: "G-9KX7SMX99J"
+  }
 };
 
 /*
