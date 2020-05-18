528bc4fb764c0c1b659b042ef40b4b14961dde4b
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-05-18 19:36:32 +0300

    Update: imported material modules into fileTaxes module

--- src/app/file-my-taxes/file-my-taxes.module.ts
+++ src/app/file-my-taxes/file-my-taxes.module.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { FileMyTaxesRoutingModule } from './file-my-taxes-routing.module';
 import { FileMyTaxesComponent } from './file-my-taxes.component';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [FileMyTaxesComponent],
   imports: [
     CommonModule,
-    FileMyTaxesRoutingModule
+    FileMyTaxesRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatSnackBarModule
   ]
 })
 export class FileMyTaxesModule { }
