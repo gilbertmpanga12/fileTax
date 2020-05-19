9bf17052be4efe8d4985035ce8fffd9f7a41a875
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-05-19 12:12:38 +0300

    Update: no changes to commit

--- src/app/file-my-taxes/file-my-taxes.module.ts
+++ src/app/file-my-taxes/file-my-taxes.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { FileMyTaxesRoutingModule } from './file-my-taxes-routing.module';
 import { FileMyTaxesComponent } from './file-my-taxes.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [FileMyTaxesComponent],
@@ -16,7 +18,10 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     MatCardModule,
     MatButtonModule,
     MatDividerModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatListModule,
+    MatIconModule,
+    MatCheckboxModule
   ]
 })
 export class FileMyTaxesModule { }
