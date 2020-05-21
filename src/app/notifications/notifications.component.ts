f1a5563c2dc1f3ca06d2e136daceb27fe8289cad
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-05-21 18:21:36 +0300

    Update: moved logic to notifications ui/ux

--- src/app/notifications/notifications.component.ts
+++ src/app/notifications/notifications.component.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Section {
+  name: string;
+  updated: Date;
+}
+
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
   styleUrls: ['./notifications.component.scss']
 })
 export class NotificationsComponent implements OnInit {
-
+  folders: Section[] = [
+    {
+      name: '5 services requested for offline filing',
+      updated: new Date('1/1/16'),
+    },
+    {
+      name: 'Completed filing Pay As You Earn',
+      updated: new Date('1/17/16'),
+    },
+    {
+      name: 'Completed filing tax returns',
+      updated: new Date('1/28/16'),
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
--- src/app/notifications/notifications.module.ts
+++ src/app/notifications/notifications.module.ts
@@ -3,13 +3,25 @@ import { CommonModule } from '@angular/common';
 
 import { NotificationsRoutingModule } from './notifications-routing.module';
 import { NotificationsComponent } from './notifications.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
   declarations: [NotificationsComponent],
   imports: [
     CommonModule,
-    NotificationsRoutingModule
+    NotificationsRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatSnackBarModule,
+    MatListModule,
+    MatIconModule
   ]
 })
 export class NotificationsModule { }
