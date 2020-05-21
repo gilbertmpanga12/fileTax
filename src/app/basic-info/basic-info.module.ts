6563c7074cf781946f75b6eb2a2ac64c9b15662b
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-05-21 11:17:25 +0300

    Update: added material module imports from basic-info module

--- src/app/basic-info/basic-info.module.ts
+++ src/app/basic-info/basic-info.module.ts
@@ -3,13 +3,30 @@ import { CommonModule } from '@angular/common';
 
 import { BasicInfoRoutingModule } from './basic-info-routing.module';
 import { BasicInfoComponent } from './basic-info.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 @NgModule({
   declarations: [BasicInfoComponent],
   imports: [
     CommonModule,
-    BasicInfoRoutingModule
+    BasicInfoRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatSnackBarModule,
+    MatListModule,
+    MatIconModule,
+    MatIconModule,
+    MatStepperModule,
+    MatTooltipModule
   ]
 })
 export class BasicInfoModule { }
