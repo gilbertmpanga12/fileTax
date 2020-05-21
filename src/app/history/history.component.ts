81b20b4194d87db64e1c2ff5ce5e4ecc632bc966
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-05-21 18:13:38 +0300

    Update: boilerplate for settingsModule completed

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,12 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Section {
+  name: string;
+  updated: Date;
+}
+
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.scss']
 })
 export class HistoryComponent implements OnInit {
-
+  folders: Section[] = [
+    {
+      name: '5 services requested for offline filing',
+      updated: new Date('1/1/16'),
+    },
+    {
+      name: 'Completed filing Pay As You Earn',
+      updated: new Date('1/17/16'),
+    },
+    {
+      name: 'Completed filing tax returns',
+      updated: new Date('1/28/16'),
+    }
+  ];
+ 
   constructor() { }
 
   ngOnInit(): void {
--- src/app/history/history.module.ts
+++ src/app/history/history.module.ts
@@ -3,13 +3,25 @@ import { CommonModule } from '@angular/common';
 
 import { HistoryRoutingModule } from './history-routing.module';
 import { HistoryComponent } from './history.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
   declarations: [HistoryComponent],
   imports: [
     CommonModule,
-    HistoryRoutingModule
+    HistoryRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatSnackBarModule,
+    MatListModule,
+    MatIconModule
   ]
 })
 export class HistoryModule { }
