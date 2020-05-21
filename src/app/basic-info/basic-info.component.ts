d8209e79d2de7d059b71e8ef4354ddaec70fd050
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-05-21 12:29:33 +0300

    Update: decalred reactive forms on basicInfo

--- src/app/basic-info/basic-info.component.ts
+++ src/app/basic-info/basic-info.component.ts
@@ -1,15 +1,78 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+/*
+personalInfo:
+-> Mother's Maiden Name
+-> Marital Status of Applicant ( Single or Married)
+-> Sex ( Male or Female)
+-> telephone
+-> Citizenship if NOT Ugandan
+-> Are you Minor ( If YES provide details of Parents / guardian)
+moreInfo:
+Have you ever been known by another name ( If yes Provide names)
+Have you previously applied for TIN ( if YES provide TIN)
 
+# residence info
+District,
+❖ Trading center,
+❖ City,
+❖ Sub County,
+❖ Parish, Village
+
+documentsUpload:
+Identification Documents required, two of the following)
+*/
 @Component({
   selector: 'app-basic-info',
   templateUrl: './basic-info.component.html',
   styleUrls: ['./basic-info.component.scss']
 })
 export class BasicInfoComponent implements OnInit {
-
-  constructor() { }
+  isLinear = false;
+  personalInfo: FormGroup;
+  moreInfo: FormGroup;
+  residenceInfo: FormGroup;
+  documentsUpload: FormGroup;
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.personalInfo = this._formBuilder.group({
+      motherMaidenName: ['', Validators.required],
+      maritalStatus: ['', Validators.required],
+      sex: ['', Validators.required],
+      telephone: ['', Validators.required],
+      citizenship: ['', Validators.required],
+      minor: ['', Validators.required]
+    });
+
+    this.moreInfo = this._formBuilder.group({
+      secondaryName: ['', Validators.required],
+      appliedForTin: ['', Validators.required],
+      partnershipCorporateId: ['', Validators.required],
+      sourceOfIncome: ['', Validators.required], // dynamic []
+      tin: ['', Validators.required]// dynamic []
+    });
+
+    this.residenceInfo = this._formBuilder.group({
+      district: ['', Validators.required],
+      city: ['', Validators.required],
+      subCounty: ['', Validators.required],
+      parish: ['', Validators.required],
+      village: ['', Validators.required]
+    });
+
+    this.documentsUpload = this._formBuilder.group({
+      nationalId: ['', Validators.required],
+      nssfCard: ['', Validators.required],
+      passport: ['', Validators.required],
+      employeeId: ['', Validators.required],
+      votersCard: ['', Validators.required],
+      workPermit: ['', Validators.required],
+      villageId: ['', Validators.required],
+      diplomaticForeignAffairsId: ['', Validators.required],
+      refugeeId: ['', Validators.required]
+    });
   }
+  
 
 }
--- src/app/basic-info/basic-info.module.ts
+++ src/app/basic-info/basic-info.module.ts
@@ -13,6 +13,7 @@ import { MatStepperModule } from '@angular/material/stepper';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [BasicInfoComponent],
@@ -29,7 +30,8 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     MatStepperModule,
     MatTooltipModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    FormsModule, ReactiveFormsModule
   ]
 })
 export class BasicInfoModule { }
