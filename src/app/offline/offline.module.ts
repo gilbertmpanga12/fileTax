fbba55fc47eeec494b1b0fe3a9bbfea38509faf4
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-05-23 16:04:44 +0300

    Update: configured all material imports for offline module

--- src/app/offline/offline.module.ts
+++ src/app/offline/offline.module.ts
@@ -3,13 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { OfflineRoutingModule } from './offline-routing.module';
 import { OfflineComponent } from './offline.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 @NgModule({
   declarations: [OfflineComponent],
   imports: [
     CommonModule,
-    OfflineRoutingModule
+    OfflineRoutingModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule
   ]
 })
 export class OfflineModule { }
