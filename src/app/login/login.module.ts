d6a6ad28184f2fdbc44ebd2c0d0ee85f47d7c522
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-05-23 23:32:21 +0300

    Update: login form completed

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,13 +3,26 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+
 
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule
   ]
 })
 export class LoginModule { }
