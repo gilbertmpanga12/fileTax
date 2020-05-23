4b3a4cedaf95b08fe84506f875dc0f7d350f52a1
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-05-23 23:55:55 +0300

    Update: minimal design for register form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Registration{
+  name: string;
+  control: string;
+}
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
+  controls: Registration[] = [{
+    name: 'First name',
+    control: 'firstName'
+  },{
+    name: 'Last name',
+    control: 'lastName'
+  }, {
+    name: 'email',
+    control: 'email'
+  },
+{
+  name: 'Address',
+  control: 'address'
+},
+{
+  name: 'Date of birth',
+  control: 'dateOfBirth'
+},
+{
+  name: 'Password',
+  control: 'password'
+}
+]
   constructor() { }
 
   ngOnInit(): void {
--- src/app/register/register.module.ts
+++ src/app/register/register.module.ts
@@ -3,13 +3,25 @@ import { CommonModule } from '@angular/common';
 
 import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterComponent } from './register.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
   declarations: [RegisterComponent],
   imports: [
     CommonModule,
-    RegisterRoutingModule
+    RegisterRoutingModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule
   ]
 })
 export class RegisterModule { }
