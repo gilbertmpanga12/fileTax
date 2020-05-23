1c26cf5aba152e6cdcfa4d10f2936646c2b2b709
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-05-23 15:57:21 +0300

    Update: completed settings page

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
 
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsComponent } from './settings.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -11,7 +13,9 @@ import { MatCardModule } from '@angular/material/card';
   imports: [
     CommonModule,
     SettingsRoutingModule,
-    MatCardModule
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class SettingsModule { }
