d4b62ed3ca5bb99c305673431c964e00ec9b2947
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-05-25 18:39:56 +0300

    Update: added progressIndicators on registration module

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormGroupDirective, NgForm, FormControl} from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 import { MainserviceService } from '../services/mainservice.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
@@ -21,8 +22,9 @@ export class RegisterComponent implements OnInit {
   registerGroup: FormGroup;
   matcher = new MyErrorStateMatcher();
   progressLoading: boolean = false;
-
-  constructor(private _fb: FormBuilder, private service: MainserviceService) { }
+  defatultText: string = 'Register';
+  loadingText: string = 'Register';
+  constructor(private _fb: FormBuilder, private service: MainserviceService, private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.registerGroup = this._fb.group({
@@ -38,18 +40,28 @@ export class RegisterComponent implements OnInit {
 
   register(): void {
     this.progressLoading = true;
+    this.loadingText = '';
     let payload = this.registerGroup.getRawValue();
     if(!this.registerGroup.invalid){
       this.service.register(payload['email'],payload['firstName'],payload['lastName'],
       payload['address'], payload['dateOfBirth'],payload['password']).then((resp) => {
         this.progressLoading = false;
+        this.loadingText = this.defatultText;
       }).catch(err => {
         this.progressLoading = false;
+        this.loadingText = this.defatultText;
+        this.snackbar(err.message);
       });
     }else{
       this.progressLoading = false;
+      this.loadingText = this.defatultText;
+     
     }
     
+  }
+
+  snackbar(message: string): void{
+    this.snackBar.open(message,'OK',{duration: 3000,verticalPosition:'top',horizontalPosition:'right'});
   }
 
 }
