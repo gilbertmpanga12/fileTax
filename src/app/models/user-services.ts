d5e1b9036b0c69748058936ae4c74ba551e01860
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-06-20 15:25:03 +0300

    Update: added taxFile services in firestore

--- src/app/models/user-services.ts
+++ src/app/models/user-services.ts
@@ -18,7 +18,7 @@ export const UserServices = {
 },
 'ura-representation': {
     serviceName: 'URA Representation',
-    description: ''
+    description: 'Attach spreadsheets, excel or pdf documents'
 },
 'tax-audit-management': {
     serviceName: 'Tax Audit Management',
@@ -27,5 +27,57 @@ export const UserServices = {
 'tax-compliance-and-advise': {
     serviceName: 'Tax Compliance & Advice',
     description: 'Attach spreadsheets, excel or pdf documents'
+},
+'vat': {
+    serviceName: 'Value Added Tax',
+    description: 'Looking for an easier way to file VAT? Start here'
 }
 };
+
+
+export const CompanyServices = {
+    'tax-compliance-advice': {
+        serviceName: 'Tax Compliance Management & Advice',
+        description: 'Attach spreadsheets, excel or pdf documents'
+    },
+    'withholding-tax-returns': {
+        serviceName: 'Withholding Tax Returns',
+        description: 'Attach spreadsheets, excel or pdf documents'
+    },
+    'rental-income-tax': {
+        serviceName: 'Rental Income Tax',
+        description: 'Attach spreadsheets, excel or pdf documents'
+    },
+    'tax-training': {
+        serviceName: 'Tax Trainings',
+        description: 'Attach spreadsheets, excel or pdf documents'
+    },
+    'business-income-tax': {
+        serviceName: 'Business Income Tax',
+        description: 'Attach spreadsheets, excel or pdf documents'
+    },
+    'advice-on-routine-tax-queries': {
+        serviceName: 'Advice on Routine Tax Queries',
+        description: 'Attach spreadsheets, excel or pdf documents'
+    },
+    'ura-representation': {
+        serviceName: 'URA Representation',
+        description: 'Attach spreadsheets, excel or pdf documents'
+    },
+    'vat': {
+        serviceName: 'Value Added Tax',
+        description: 'Attach spreadsheets, excel or pdf documents'
+    },
+    'tax-efficient-ways': {
+        serviceName: 'Tax Efficient ways to structure business transactions',
+        description: 'Attach spreadsheets, excel or pdf documents'
+    },
+    'tax-audit-management': {
+        serviceName: 'Tax Audit Management',
+        description: 'Attach spreadsheets, excel or pdf documents'
+    },
+    'pay-as-you-earn': {
+        serviceName: 'Pay As You Earn',
+        description: 'File your employees\' PAYE instantly attach documents'
+    }
+    };
