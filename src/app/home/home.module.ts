d43953b02109aebed294bd7d2a2d814228e5816b
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-06-24 21:28:48 +0300

    Update: finished making minimal changes

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,15 +7,23 @@ import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import { UserdashboardComponent } from './userdashboard/userdashboard.component';
 import { CompanydasboardComponent } from './companydasboard/companydasboard.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { UserHistoryComponent } from './user-history/user-history.component';
+import { CompanyHistoryComponent } from './company-history/company-history.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, UserdashboardComponent, CompanydasboardComponent],
+  declarations: [HomeComponent, UserdashboardComponent, CompanydasboardComponent,
+  UserHistoryComponent, CompanyHistoryComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
   ]
 })
 export class HomeModule { }
