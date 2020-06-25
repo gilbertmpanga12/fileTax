0908be5292189f56a78efdcf2ff7184b951e7b5b
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-06-25 12:58:08 +0300

    Update: making changes to landingPages 50%;

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,20 @@
 import * as functions from 'firebase-functions';
-
+const sgMail = require('@sendgrid/mail');
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
 export const setBusinessAccount = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+  const toEmail = request.params['toEmail'];
+  const fromEmail = request.params['fromEmail'];
+  const subject = request.params['subject'];
+  const message  = request.params['message'];
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+  to: toEmail,
+  from: fromEmail,
+  subject: 'Sending with Twilio SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+sgMail.send(msg);
 });
