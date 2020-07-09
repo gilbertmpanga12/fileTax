9f4979bce2d0357654784d31f4191ac2f098d590
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-07-09 15:47:28 +0300

    Update; minimal design completed

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,11 +7,25 @@ import { MainserviceService } from '../services/mainservice.service';
 import { TaxNotifications, IndividualUser } from '../models/datamodels';
 import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection} from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+
+declare const FlutterwaveCheckout: any;
 // Models
 interface Links {
   path: string;
   icon: string;
   name: string;
+}
+// interface features{
+//   feature1: string;
+//   feature2: string;
+//   feature3: string;
+//   feature4: string;
+// }
+interface Plans{
+  title: string;
+  price: string;
+  features: string[];
+ 
 }
 
 @Component({
@@ -22,6 +36,15 @@ interface Links {
 export class DashboardComponent implements OnInit {
   date: Date = new Date();
   year: number = this.date.getFullYear();
+  plans: Plans[] = [{title: 'Starter Pack',features: ['Online Filing of Taxes',
+'Online Support', 'Tax Consultation not included', 'Client Visits not included'
+],price: 'UGX50,000'},
+  {title: 'SME Pack',features: ['Online Filing of Taxes', 
+  'Online Support', 'Tax Consultation', 'Client Visits not included'],price: 'UGX70,000'},
+
+  {title: 'Onsite Pack',features: ['Client Visits', 
+  'Online/Offline Filing of Taxes', 'Online Support', 'Tax Consultation & more'],price: 'UGX70,000'}
+];
 links:  Links[] = [{
   path: '/',
   icon: 'icon ni ni-shop icon-lg icon-outline icon-stroke-3 ni-2x',
@@ -69,7 +92,7 @@ constructor(private breakpointObserver: BreakpointObserver, private router: Rout
 }
 
   ngOnInit(): void {
-   
+    this.loadScript('https://checkout.flutterwave.com/v3.js');
   }
   checkNotifications(): void{
     // do something after navigation
@@ -80,6 +103,46 @@ constructor(private breakpointObserver: BreakpointObserver, private router: Rout
 
   logout(): void{
     this.service.logout();
+  }
+
+  subscribe(): void{
+    console.log('fewms');
+    FlutterwaveCheckout({
+      public_key: "FLWPUBK_TEST-31d61a13026483fc38f15f0e90232374-X",
+      tx_ref: "hooli-tx-1920bbtyt",
+      amount: 54600,
+      currency: "NGN",
+      payment_options: "card,mobilemoney,ussd",
+      redirect_url: // specified redirect URL
+        "https://callbacks.piedpiper.com/flutterwave.aspx?ismobile=34",
+      meta: {
+        consumer_id: 23,
+        consumer_mac: "92a3-912ba-1192a",
+      },
+      customer: {
+        email: "user@gmail.com",
+        phone_number: "08102909304",
+        name: "yemi desola",
+      },
+      callback: function (data) {
+        console.log(data);
+      },
+      customizations: {
+        title: "My store",
+        description: "Payment for items in cart",
+        logo: "https://assets.piedpiper.com/logo.png",
+      },
+    });
+  }
+
+  public loadScript(url: string) {
+    const body = <HTMLDivElement> document.body;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = url;
+    script.async = false;
+    script.defer = true;
+    body.appendChild(script);
   }
 
 }
